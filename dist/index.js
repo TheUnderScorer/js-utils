68cb7126dd558394649b73231b8a73416741b8a0
Przemysław Żydek <przemyslaw.zydek@tsh.io>
2019-12-22 18:47:10 +0100

    Update exports

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,7 @@
 "use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
 Object.defineProperty(exports, "__esModule", { value: true });
-const reducer_1 = require("./reducer");
-exports.reducer = reducer_1.default;
+__export(require("./reducer"));
 //# sourceMappingURL=index.js.map
--- dist/reducer/index.d.ts
+++ dist/reducer/index.d.ts
@@ -2,5 +2,5 @@ import Action from '../actions/interfaces/Action';
 /**
  *  Builds reducer basing on provided handlers (that are connected to action types) and initial state
  * */
-declare const reducer: <State, Actions extends Action<any>>(handlers: Record<Actions["type"], import("./types/ReducerHandler").ReducerHandler<State>>, initialState: State) => (state: State, action: Action<any>) => State;
+declare const reducer: <State, Actions extends Action<any>>(handlers: Record<Actions["type"], import("..").ReducerHandler<State>>, initialState: State) => (state: State, action: Action<any>) => State;
 export default reducer;
